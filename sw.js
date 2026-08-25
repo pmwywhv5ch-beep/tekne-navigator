7768929a7127b56262bc7db32bd384c2ad5876a3
pmwywhv5ch-beep <pmwywhv5ch@privaterelay.appleid.com>
2026-08-25 17:49:06 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tekne-navigator-v15';
+const CACHE='tekne-navigator-v16';
 const APP=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./waypoints.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
