d86318a6056a3702127d6bfd63a2e56496d26fc2
pmwywhv5ch-beep <pmwywhv5ch@privaterelay.appleid.com>
2026-08-27 16:40:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='tekne-navigator-v19';
-const APP=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./waypoints.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const c=x.clone();caches.open(CACHE).then(ca=>ca.put(e.request,c));return x}).catch(()=>caches.match('./index.html'))));});
+const CACHE='tekne-navigator-v20';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
